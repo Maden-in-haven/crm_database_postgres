420f25fe0dd71677f1dcd6779468171251e3088b
Azreil <68663142+Azreil-OFD@users.noreply.github.com>
2024-09-24 06:52:55 +0500

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -2,7 +2,6 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(100) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL CHECK (role IN ('Admin', 'Client', 'Manager')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -12,24 +11,22 @@ CREATE TABLE users (
 -- Создание таблицы для администраторов
 CREATE TABLE admins (
     id UUID PRIMARY KEY REFERENCES users(id),
-    permissions JSONB, -- Поле для хранения прав доступа в формате JSON
+    permissions JSONB,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Создание таблицы для клиентов
 CREATE TABLE clients (
     id UUID PRIMARY KEY REFERENCES users(id),
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+    full_name VARCHAR(100),
     phone_number VARCHAR(15),
-    address TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Создание таблицы для менеджеров
 CREATE TABLE managers (
     id UUID PRIMARY KEY REFERENCES users(id),
-    department VARCHAR(100),
+    full_name VARCHAR(100),
     hire_date TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
